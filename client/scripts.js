b9cb3eeb4a8f881ed6aac57d50f7a923bbbaa9ce
SmartTechComputerSolutions <108148152+SmartTechComputerSolutions@users.noreply.github.com>
2024-10-06 16:33:12 -0400

    Add files via upload

---
+++ client/scripts.js
@@ -0,0 +1,159 @@
+var zodiacs = [
+    {
+        name: "Aries",
+        image: "/imgs/aries.jpg",
+        month: "(March 21-April 20)",
+        description: "Aries are driven by a desire to prove themselves and their strength. They have high energy, and are competitive and ambitious. They naturally take charge because they are good at initiating new projects. They can also be impatient, but are naturally active and don't like to waste time. "
+    },
+    {
+        name: "aries",
+        image: "/imgs/aries.jpg",
+        month: "(March 21-April 20)",
+        description: "Aries are driven by a desire to prove themselves and their strength. They have high energy, and are competitive and ambitious. They naturally take charge because they are good at initiating new projects. They can also be impatient, but are naturally active and don't like to waste time. "
+        
+
+    },
+    {
+        name: "Taurus",
+        image: "/imgs/taurus.jpg",
+        month: "(April 20-May 20)",
+        description: "Tauruses have a regal quality about them. They are graceful and diligent laborers. They can be stubborn, bull-headed, and set in their ways, but they are also great listeners and very dependable. Tauruses can absolutely go on forever."
+    },
+    {
+        name: "taurus",
+        image: "/imgs/taurus.jpg",
+        month: "(April 20-May 20)",
+        description: "Tauruses have a regal quality about them. They are graceful and diligent laborers. They can be stubborn, bull-headed, and set in their ways, but they are also great listeners and very dependable. Tauruses can absolutely go on forever."
+    },
+    {
+        name: "Gemini",
+        image: "/imgs/gemini.jpg",
+        month: "(May 21-June 20)",
+        description: "Playful and intellectually curious, Gemini is constantly juggling a variety of passions, hobbies, careers, and friend groups. They are the social butterflies of the zodiac: These quick-witted twins can talk to anyone about anything. Find them buzzing between happy hours, dinner parties, and dance floors."
+    },
+    {
+        name: "gemini",
+        image: "/imgs/gemini.jpg",
+        month: "(May 21-June 20)",
+        description: "Playful and intellectually curious, Gemini is constantly juggling a variety of passions, hobbies, careers, and friend groups. They are the social butterflies of the zodiac: These quick-witted twins can talk to anyone about anything. Find them buzzing between happy hours, dinner parties, and dance floors."
+    },
+    {
+        name: "Cancer",
+        image: "/imgs/cancer.jpg",
+        month: "(June 21 to July 22)",
+        description: "Typical sun in Cancer traits include being nurturing, sensitive, compassionate, self-protective, security-seeking, loving, and displaying a goofy, ingratiating sense of humor."
+    },
+    {
+        name: "cancer",
+        image: "/imgs/cancer.jpg",
+        month: "(June 21 to July 22)",
+        description: "Typical sun in Cancer traits include being nurturing, sensitive, compassionate, self-protective, security-seeking, loving, and displaying a goofy, ingratiating sense of humor."
+    },
+    {
+        name: "Leo",
+        image: "/imgs/leo.jpg",
+        month: "(July 23 to August 22)",
+        description: " Leos are noted for being lively, fun, loyal, and honest in their individual connections, demonstrating their enthusiasm for life. From their love relationships, Leos want deep devotion, focused attention at the proper times, and experience"
+    },
+    {
+        name: "leo",
+        image: "/imgs/leo.jpg",
+        month: "(July 23 to August 22)",
+        description: " Leos are noted for being lively, fun, loyal, and honest in their individual connections, demonstrating their enthusiasm for life. From their love relationships, Leos want deep devotion, focused attention at the proper times, and experience"
+    },
+    {
+        name: "Virgo",
+        image: "/imgs/virgo.jpg",
+        month: "(August 23 to September 22)",
+        description: "This earth sign is a perfectionist at heart and isn't afraid to improve skills through diligent and consistent practice. Virgo rules the digestive system, which makes these earth signs especially attuned to the ingredients that make up a whole—in food and in everything else. They're hyper-aware of every detail."
+    },
+    {
+        name: "virgo",
+        image: "/imgs/virgo.jpg",
+        month: "(August 23 to September 22)",
+        description: "This earth sign is a perfectionist at heart and isn't afraid to improve skills through diligent and consistent practice. Virgo rules the digestive system, which makes these earth signs especially attuned to the ingredients that make up a whole—in food and in everything else. They're hyper-aware of every detail."
+    },
+    {
+        name: "Libra",
+        image: "/imgs/libra.jpg",
+        month: "(September 23 to October 22)",
+        description: "Libra is obsessed with symmetry and strives to create equilibrium in all areas of life. These air signs are the aesthetes of the zodiac: Ruled by Venus, the planet that governs love, beauty, and money, Libras adore high art, intellectualism, and connoisseurship."
+    },
+    {
+        name: "libra",
+        image: "/imgs/libra.jpg",
+        month: "(September 23 to October 22)",
+        description: "Libra is obsessed with symmetry and strives to create equilibrium in all areas of life. These air signs are the aesthetes of the zodiac: Ruled by Venus, the planet that governs love, beauty, and money, Libras adore high art, intellectualism, and connoisseurship."
+    },
+    {
+        name: "Scorpio",
+        image: "/imgs/scorpio.jpg",
+        month: "(October 23 to Noverber 22)",
+        description: "As a fixed water sign, Scorpios are known for their loyalty and devotion — and also their passion. In fact, they are known to have such intense personalities that they are often confused with vibrant fire signs. Alas, they just have deep feelings and emotions"
+    },
+    {
+        name: "scorpio",
+        image: "/imgs/scorpio.jpg",
+        month: "(October 23 to Noverber 22)",
+        description: "As a fixed water sign, Scorpios are known for their loyalty and devotion — and also their passion. In fact, they are known to have such intense personalities that they are often confused with vibrant fire signs. Alas, they just have deep feelings and emotions"
+    },
+    {
+        name: "Sagittarius",
+        image: "/imgs/sagittarius.jpg",
+        month: "(Novermber 22 to December 21)",
+        description: "What makes Sagittarius so unique is its dynamic blend of passion, curiosity, intensity, and adaptability. Represented by the archer (a half-man, half-horse centaur), Sagittarius isn't afraid to use its bow and arrow to explore expansive terrain, seeking answers in places and spaces others wouldn't dare venture."
+    },
+    {
+        name: "sagittarius",
+        image: "/imgs/sagittarius.jpg",
+        month: "(Novermber 22 to December 21)",
+        description: "What makes Sagittarius so unique is its dynamic blend of passion, curiosity, intensity, and adaptability. Represented by the archer (a half-man, half-horse centaur), Sagittarius isn't afraid to use its bow and arrow to explore expansive terrain, seeking answers in places and spaces others wouldn't dare venture."
+    },
+    {
+        name: "Capricorn",
+        image: "/imgs/capricorn.jpg",
+        month: "(December 22 to January 19)",
+        description: "Stardust describes Capricorns as “hardworking, persistent, creative, honest, reflective, protective and sensitive.” Because they are earth signs, she adds, they “relish the material and physical world.” She also notes that Capricorns are a “cardinal” sign, meaning that they initiate the start of a season."
+    },
+    {
+        name: "capricorn",
+        image: "/imgs/capricorn.jpg",
+        month: "(December 22 to January 19)",
+        description: "Stardust describes Capricorns as “hardworking, persistent, creative, honest, reflective, protective and sensitive.” Because they are earth signs, she adds, they “relish the material and physical world.” She also notes that Capricorns are a “cardinal” sign, meaning that they initiate the start of a season."
+    },
+    {
+        name: "Aquarius",
+        image: "/imgs/aquarius.jpg",
+        month: "(January 20-February 18)",
+        description: "Aquarians are highly intellectual and creative, Walker says. Marked by independence, they don't like to be instructed what to do. While they can be social, they are not likely to participate in social interactions unless they truly want to. Aquarians are ideas people, never suffering a drought of inspiration."
+    },
+    {
+        name: "aquarius",
+        image: "/imgs/aquarius.jpg",
+        month: "(January 20-February 18)",
+        description: "Aquarians are highly intellectual and creative, Walker says. Marked by independence, they don't like to be instructed what to do. While they can be social, they are not likely to participate in social interactions unless they truly want to. Aquarians are ideas people, never suffering a drought of inspiration."
+    },
+    {
+        name: "Pisces",
+        image: "/imgs/pisces.jpg",
+        month: "(February 19 to March 20)",
+        description: "Pisces are highly creative and imaginative, Walker reports, as well as compassionate and loving. Their emotional sensitivity factor is high, helping them to remain in tune with others but also leaving them vulnerable to criticism, worrying a lot about the effect that their actions might have on others."
+    },
+    {
+    name: "pisces",
+    image: "/imgs/pisces.jpg",
+    month: "(February 19 to March 20)",
+    description: "Pisces are highly creative and imaginative, Walker reports, as well as compassionate and loving. Their emotional sensitivity factor is high, helping them to remain in tune with others but also leaving them vulnerable to criticism, worrying a lot about the effect that their actions might have on others."
+}
+]
+function getData() {
+    var userSearch = document.getElementById('user-search').value;
+    for(var i = 0; i < zodiacs.length; i++){
+        if(userSearch === zodiacs[i].name){
+            document.getElementById('img').src = zodiacs[i].image;
+            document.getElementById('name').innerHTML = zodiacs[i].name;
+            document.getElementById('month').innerHTML = zodiacs[i].month;
+            document.getElementById('text-result').innerHTML = zodiacs[i].description;
+        }
+    }
+};
